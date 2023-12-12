9a9a99fa8ee281bcba2b663eebada9b3e9742e94
muhammadumar0820 <salimjonovmuhammadumar372@gmail.com>
2023-12-12 18:41:40 +0300

    darslar bajarildi

---
+++ index.js
@@ -0,0 +1,119 @@
+//Birinchi masala
+//let num1 = 32;
+//let num2 = 20;
+//let num3 = 10;
+
+//let fiver;
+//counter = 0;
+
+//if (num1 % 2 == 0 && num1 % 5 == 0) {
+//    fiver = num1;
+//    counter = counter+10;
+//}
+
+//if (num2 % 2 == 0 && num2 % 5 == 0) {//
+//    fiver = num2;
+//    counter = counter+10;
+//}
+
+//if (num3 % 2 == 0 && num3 % 5 == 0) {//
+//    fiver = num3;
+//    counter = counter+10;
+//}
+
+//console.log(counter*fiver);
+
+
+
+//Ikkinchi masala
+//let num1 = 42;
+//let num2 = 20;
+//let num3 = 10;
+//let num4 = 21;
+
+//let fiver;
+//counter = 0;
+
+//if (num1 % 3 == 0 && num1 % 7 == 0) {
+//   fiver = num1;
+//    counter = counter+21;
+//}
+
+//if (num2 % 3 == 0 && num2 % 7 == 0) {//
+//  fiver = num2;
+//  counter = counter+21;
+//}
+
+//if (num3 % 3 == 0 && num3 % 7 == 0) {//
+//    fiver = num3;
+//    counter = counter+21;
+//}
+
+//if (num4 % 3 == 0 && num4 % 7 == 0) {//
+//    fiver = num4;
+//    counter = counter+21;
+//}
+
+//console.log(counter*fiver);
+
+
+
+
+//Uchinchi masala
+//let num1 = 42;
+
+//let ten = 4;
+//let one = 2;
+
+//console.log(ten*one);
+
+
+
+
+
+//To'rtinchi masala
+
+//let num1 = 52;
+
+//let res = 5;
+
+//if (res % 2 == 0){
+//    console.log(res**2);
+///}else{
+//    console.log(res**3);
+//}
+
+
+
+
+//Beshunchi masala
+//let num1 = 213;
+//num1 = num1/100;
+
+//console.log(Math.trunc(num1));
+
+
+//Yettinchi masala
+let num1 = 1134;
+let one = num1/1;
+let ten = num1/10
+let hundred = num1/100;
+let thousand = num1/1000;
+let counter = 0;
+if(one%2==0){
+    counter=counter+1;
+}
+
+if(ten%2==0){
+    counter=counter+1;
+}
+
+if(hundred%2==0){
+    counter=counter+1;
+}
+
+if(thousand%2==0){
+    counter=counter+1;
+}
+
+console.log(counter);
